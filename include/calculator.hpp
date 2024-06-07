64f90673b52f73b0204199b59c4cde6cc9f133d6
gates02 <gates02@naver.com>
2024-06-07 05:37:01 +0000

    minor change

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -1,5 +1,10 @@
-class Calculator{
-    public:
-        int Add(int a, int b){};
-        int Multiply(int a, int b){};
+#ifndef CALCULATOR_HPP
+#define CALCULATOR_HPP
+
+class Calculator {
+public:
+	int Add(int a, int b);
+	int Multiply(int a, int b);
 };
+
+#endif // CALCULATOR_HPP
